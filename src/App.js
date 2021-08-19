41fa32652184950f2f3d092fc94d812d59e73cfa
RowdyKGZ <rowdykg@gmail.com>
2021-08-19 15:17:21 +0600

    Верстка все

--- src/App.js
+++ src/App.js
@@ -3,6 +3,77 @@ import "./App.css";
 function App() {
   return (
     <div className="wrapper clear">
+      {/*  */}
+      <div className="overlay">
+        <div className="drawer d-flex flex-column">
+          <h2 className="d-flex justify-between mb-30">
+            Корзина{" "}
+            <img
+              className="removeBtn cu-p"
+              src="/img/btn-remove.svg"
+              alt="remove"
+            />
+          </h2>
+
+          {/*  */}
+          <div className="items flex">
+            {/*  */}
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+            </div>
+
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/2.jpg)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+            </div>
+          </div>
+
+          {/*  */}
+
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого: </span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1874 руб.</b>
+              </li>
+            </ul>
+
+            <button className="greenButton">
+              Оформить заказ <img src="/img/arrow.svg" alt="arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" alt="logo" />
